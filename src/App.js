1f590c0a82bead840a22b9e2715deb6b7ed8c7ee
fareedbaloch1 <41841944+fareedbaloch1@users.noreply.github.com>
2022-05-04 17:17:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Project Portfolio Website
+          Fareed Portfolio Website
         </p>
         <a
           className="App-link"
